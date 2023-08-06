b912490c73f686a103285aa835524d4dcbb623ba
Regestea <amirhossein5starstar@outlook.com>
2023-08-06 16:42:59 +0330

    Test for File Upload Service finished

--- Services/ObjectStorage/ObjectStorage.Api.Test/Services/FileUploadServiceTest.cs
+++ Services/ObjectStorage/ObjectStorage.Api.Test/Services/FileUploadServiceTest.cs
@@ -1,29 +1,25 @@
 ﻿using Azure.Storage.Blobs;
-using Microsoft.AspNetCore.Mvc;
 using Moq;
 using ObjectStorage.Api.Context;
-using ObjectStorage.Api.Controllers;
 using ObjectStorage.Api.Entities;
-using ObjectStorage.Api.Services.InterFaces;
-using System.IO;
 using Azure.Storage.Blobs.Models;
-using Microsoft.Extensions.Azure;
 using ObjectStorage.Api.Services;
 using Xunit.Abstractions;
+using ObjectStorage.Api.Test.DataGenerator;
 
-namespace ObjectStorage.Api.Test;
+namespace ObjectStorage.Api.Test.Services;
 
-public class ObjectStorageControllerTests
+public class FileUploadServiceTest
 {
     private const string BlobTestServer = "AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;";
     private readonly ITestOutputHelper _testOutputHelper;
 
-    public ObjectStorageControllerTests(ITestOutputHelper testOutputHelper)
+    public FileUploadServiceTest(ITestOutputHelper testOutputHelper)
     {
         _testOutputHelper = testOutputHelper;
     }
 
-    
+
     [Fact]
     public async Task Upload_Should_UploadStreamToBlobStorage()
     {
@@ -36,9 +32,6 @@ public class ObjectStorageControllerTests
         var fileStream = TextToImageStream.ConvertTextToImageStream("test image");
 
         var fileUploadService = new FileUploadService(mockBlobClientFactory.Object);
-        
-        
-        _testOutputHelper.WriteLine($"This is stream length: {(double)fileStream.Length / 1024} KB");
 
         // Act
 
@@ -53,4 +46,32 @@ public class ObjectStorageControllerTests
         var blobClient = new BlobContainerClient(BlobTestServer, ContainerName.image.ToString());
         await blobClient.DeleteBlobIfExistsAsync(result);
     }
+
+    [Fact]
+    public async Task Delete_Should_DeleteObjectFromBlobStorage()
+    {
+        // Arrange
+        var mockBlobClientFactory = new Mock<IBlobClientFactory>();
+
+        mockBlobClientFactory.Setup(f => f.BlobStorageClient(ContainerName.image))
+            .Returns(() => new BlobContainerClient(BlobTestServer, ContainerName.image.ToString()));
+
+        var fileStream = TextToImageStream.ConvertTextToImageStream("test image");
+
+        var fileUploadService = new FileUploadService(mockBlobClientFactory.Object);
+
+        // Act
+
+        var uploadResult = await fileUploadService.UploadObjectAsync(ContainerName.image, "Test.png", fileStream, AccessTier.Archive,
+            CancellationToken.None);
+        var deleteResult = await fileUploadService.DeleteObjectAsync(ContainerName.image, uploadResult);
+
+        // Assert
+        Assert.NotNull(uploadResult);
+        Assert.IsType<string>(uploadResult);
+        Assert.NotNull(deleteResult);
+        Assert.IsType<bool>(deleteResult);
+        Assert.Equal(true, deleteResult);
+    }
+
 }

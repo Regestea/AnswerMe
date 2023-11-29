5ca1539cf19fd61330c2ffc9e9fba7d685cee62a
Regestea <amirhossein5starstar@outlook.com>
2023-11-29 14:53:51 +0330

    messages design finished

--- Client/AnswerMe.App/AnswerMe.App/wwwroot/scriptJs/app.js
+++ Client/AnswerMe.App/AnswerMe.App/wwwroot/scriptJs/app.js
@@ -1,15 +1,22 @@
-﻿
-
+﻿//do all of them in js if user was at home page and the screen was too small full screen the side bar
 var GLOBAL = {};
 GLOBAL.DotNetReference = null;
+
 function setDotnetReference(pDotNetReference) {
     GLOBAL.DotNetReference = pDotNetReference;
 }
 
 window.addEventListener('resize', function () {
     const sizeCategory = getScreenSizeCategory();
-    GLOBAL.DotNetReference.invokeMethodAsync( 'OnWindowResized', sizeCategory);
+    const isAtHome=IsAtHomePage();
+    document.getElementById("NavMenu");
+    document.getElementById("MainBody");
 });
+
+function IsAtHomePage() {
+    // Check if the user is on the home page
+    return window.location.pathname === '/';
+}
 
 function getScreenSizeCategory() {
     const width = window.innerWidth;

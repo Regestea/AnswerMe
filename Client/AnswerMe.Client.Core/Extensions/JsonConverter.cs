cc8358e3f21cfecf36b74f82e3e6132cf4e8f6a9
Regestea <amirhossein5starstar@outlook.com>
2023-12-18 22:58:38 +0330

    it almost work

--- Client/AnswerMe.Client.Core/Extensions/JsonConverter.cs
+++ Client/AnswerMe.Client.Core/Extensions/JsonConverter.cs
@@ -14,7 +14,7 @@ namespace AnswerMe.Client.Core.Extensions
             PropertyNameCaseInsensitive = true,
         };
 
-        public static Task<T> ToObject<T>(string content) 
+        public static Task<T> ToObject<T>(string content)
         {
             return Task.FromResult(JsonSerializer.Deserialize<T>(content, _options) ??
                                    throw new InvalidOperationException());
@@ -31,21 +31,52 @@ namespace AnswerMe.Client.Core.Extensions
             return Task.FromResult(JsonSerializer.Serialize(content, _options));
         }
 
-        public static Task<List<ValidationFailed>> ToValidationFailedList(string content)
+        public static async Task<List<ValidationFailed>> ToValidationFailedList(string content)
         {
-            var errorDict = JsonSerializer.Deserialize<Dictionary<string, List<string>>>(content);
-            var validationFailedList = new List<ValidationFailed>();
+            try
+            {
+                var errorDict = JsonSerializer.Deserialize<Dictionary<string, List<string>>>(content);
+                var validationFailedList = new List<ValidationFailed>();
 
-            if (errorDict != null)
-                foreach (var error in errorDict)
-                {
-                    foreach (string errorMessage in error.Value)
+                if (errorDict != null)
+                    foreach (var error in errorDict)
                     {
-                        validationFailedList.Add(new ValidationFailed() { Field = error.Key, Error = errorMessage });
+                        foreach (string errorMessage in error.Value)
+                        {
+                            validationFailedList.Add(new ValidationFailed() { Field = error.Key, Error = errorMessage });
+                        }
+                    }
+
+                return validationFailedList;
+            }
+            catch (Exception e)
+            {
+                JsonDocument jsonDocument = JsonDocument.Parse(content);
+
+                // Navigate to the "errors" field
+                JsonElement errorsElement = jsonDocument.RootElement.GetProperty("errors");
+
+                // Check if the "errors" field is an object
+                if (errorsElement.ValueKind == JsonValueKind.Object)
+                {
+                    // Access the value of the "FileFormat" key
+                    JsonElement fileFormatElement = errorsElement.GetProperty("FileFormat");
+
+                    // Check if the "FileFormat" value is an array
+                    if (fileFormatElement.ValueKind == JsonValueKind.Array)
+                    {
+                        // Access the first element of the array
+                        JsonElement firstErrorElement = fileFormatElement.EnumerateArray().FirstOrDefault();
+
+                        // Get the value as a string
+                        string firstErrorValue = firstErrorElement.GetString();
+
+                        return new List<ValidationFailed>() {new ValidationFailed(){Field = "error",Error = firstErrorValue} };
                     }
                 }
+            }
 
-            return Task.FromResult(validationFailedList);
+            return new List<ValidationFailed>() { new ValidationFailed(){Field = "error",Error = "some error happend"} };
         }
     }
 }
